fea91e82c39e535de4ef7b861867530e939f6327
Sidi Merzouk <sidimerzouk@MacBook-Pro.home>
2020-08-12 17:01:55 -0400

    Add some logic

--- actions/index.js
+++ actions/index.js
@@ -1,26 +1,42 @@
 const { Octokit } = require("@octokit/rest");
 const github = require("@actions/github");
 
-// require("dotenv").config();
+require("dotenv").config();
 const { env } = process;
-const owner = "Sid-ah";
-const repo = "hygiene";
+const owner = "bklynproject";
+const repo = "soccer";
 let auth = env.gh_token;
 
-console.log("auth ", auth);
 const octokit = new Octokit({ auth });
 
 async function getIssues() {
   try {
     const options = octokit.issues.listForRepo.endpoint.merge({
-      owner,
       repo,
+      owner,
       state: "open",
     });
 
-    const data = await octokit.paginate(options);
+    const openIssues = await octokit.paginate(options);
+    const issueNumbers =
+      openIssues.length > 0 ? openIssues.map((issue) => issue.number) : [];
 
-    console.log("data ", data);
+    const q = `state:open+project:${owner}/${repo}/1`;
+    const dataFromBoard = await octokit.search.issuesAndPullRequests({
+      q,
+    });
+    const { items } = dataFromBoard.data;
+    const issueNumbersFromBoards =
+      items.length > 0 ? items.map((item) => item.number) : [];
+    let temp = {};
+    for (let number of issueNumbers) {
+      if (!issueNumbersFromBoards.includes(number)) {
+        temp[number] = false;
+      } else {
+        temp[number] = true;
+      }
+    }
+    console.log("temp ", temp);
   } catch (err) {
     console.log("err ", err);
   }
